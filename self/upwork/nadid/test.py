ed9df5889c4c26ce42834a8c813e8ef278323906
ANKIT <ankit.rawat4685@gmail.com>
2021-11-26 20:34:56 +0800

    Upword - Nanid -  MA strategy - Completed.

--- self/upwork/nadid/test.py
+++
@@ -1,8 +0,0 @@
-import pandas
-import pandas as pd
-import numpy as np
-portfolio =  pd.DataFrame(columns=["portfolioStocksPosition", "PNL", "MTM"])
-
-df=pd.DataFrame(np.zeros([1,3]),columns=["portfolioStocksPosition", "PNL", "MTM"])
-
-print(df)
